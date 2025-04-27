9ca8fb28a9c4fc0b0a427ea1a8d0fad1591ef30e
dheeraj9489 <dheerajmb19@gmail.com>
2025-04-26 21:48:45 -0500

    Added sound and ray impact

--- 3.42/Assets/RayGun.cs
+++ 3.42/Assets/RayGun.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.SymbolStore;
+
 //using System.Diagnostics;
 using UnityEngine;
 
 public class RayGun : MonoBehaviour
 {
+    public LayerMask layerMask;
     public OVRInput.RawButton shootingButton;
     public GameObject linePrefab;
+    public GameObject rayImpactPrefab;
     public Transform shootingPoint;
     public float maxLineDistance = 5;
     public float lineShowTimer = 0.3f;
+    public AudioSource source;
+    public AudioClip shootingAudioClip;
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +36,31 @@ public class RayGun : MonoBehaviour
 
     public void Shoot()
     {
-        Debug.Log("Pew Pew");
+        source.PlayOneShot(shootingAudioClip);
+
+        Ray ray = new Ray(shootingPoint.position, shootingPoint.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layerMask);
+        Vector3 endPoint = Vector3.zero;
+
+        if (hasHit)
+        {
+            // stop the ray
+            endPoint = hit.point;
+
+            Quaternion rayImpactRotation = Quaternion.LookRotation(-hit.normal); 
+            GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point, rayImpactRotation);
+            Destroy(rayImpact, 1);
+        }
+        else
+        {
+            endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        }
+
 
         LineRenderer line = Instantiate(linePrefab).GetComponent<LineRenderer>();
         line.positionCount = 2;
         line.SetPosition(0, shootingPoint.position);
 
-        Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
         line.SetPosition(1, endPoint);
 
         Destroy(line.gameObject, lineShowTimer);
